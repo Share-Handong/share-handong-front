6bb17daa2215935de31d355935d2cbaa3c761df4
ohh31 <saewon412@gmail.com>
2021-04-30 20:38:02 +0900

    feat: 임시 share 라우팅 버튼 추가

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from 'next/link'
+
 export default function Home() {
+
   return (
     <div>
       <h1>Share-handong-front</h1>
+      <Link href = '/share'>
+      <button>
+        ho
+      </button>
+      </Link>
+   
     </div>
   );
 }
