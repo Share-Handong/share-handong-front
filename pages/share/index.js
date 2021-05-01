e7d8c7fc36ea98dd3e9cc2832b699609d169d86b
ohh31 <saewon412@gmail.com>
2021-05-01 15:17:55 +0900

    fix: category 선택 기능 추가 및 api 데이터 전송

--- pages/share/index.js
+++ pages/share/index.js
@@ -1,6 +1,18 @@
 import "semantic-ui-css/semantic.min.css";
+import { Divider } from "semantic-ui-react";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import React, { useEffect, useState } from "react";
 
 export default function ShareForm() {
+  const [category, setCategory] = useState('');
+ 
+  const handleChange = async event => {
+    event.preventDefault();
+    setCategory(event.target.value);
+  }
+  
   const createPost = async event => {
     event.preventDefault()
 
@@ -9,7 +21,8 @@ export default function ShareForm() {
       {
         body: JSON.stringify({
           title: event.target.title.value,
-          desc : event.target.desc.value
+          desc : event.target.desc.value,
+          category: category,
         }),
         headers: {
           'Content-Type': 'application/json'
@@ -18,6 +31,9 @@ export default function ShareForm() {
       }
     ).then((res) => res.json());
   }
+
+ 
+
     return( 
     <article style={{
       // width: "91.6%",
@@ -35,16 +51,35 @@ export default function ShareForm() {
               paddingRight:"65px",
               zIndex:1,
             }} >
+   
  <form action="/create_process" method="post" onSubmit={createPost}>
-      <p><input id = "title" name="title" type="text"></input></p>
+ <select id="category" onChange={handleChange} value={category}>
+      <option selected value = "나눔받기">나눔받기</option>
+      <option value = "나눔하기">나눔하기</option>
+    </select>
+      <p><input id = "title" name="title" type="text" style={{
+              backgroundColor : "white",
+              height: "92px",
+              width: "644px",
+              fontSize : "45px",
+              // marginLeft: "88px",
+              // marginRight: "88px",
+              zIndex:1,
+            }}></input></p>
+      <Divider />
       <p>
-        <textarea id = "desc" name="desc" type="text"  ></textarea>
+        <textarea id = "desc" name="desc" type="text" style={{
+              backgroundColor : "white",
+              height: "482px",
+              width: "1128px",
+              zIndex:1,
+            }} ></textarea>
       </p>
+      
       <p>
         <button type="submit" >제출</button>
       </p>
     </form>
-   
 </div>
 </article>
 
