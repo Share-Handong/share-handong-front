c840fd2e51fa9159ceb9d5965d5357851f0f4ba6
ohh31 <saewon412@gmail.com>
2021-05-08 23:24:40 +0900

    feat : share 게시물 페이지 margin, padding 추가

--- pages/share/index.js
+++ pages/share/index.js
@@ -4,6 +4,7 @@ import { Divider, Placeholder } from "semantic-ui-react";
 import ReplyIcon from '@material-ui/icons/Reply';
 import CreateIcon from '@material-ui/icons/Create';
 import DescriptionIcon from '@material-ui/icons/Description';
+import { Rowing } from '@material-ui/icons';
 
 export default function Share() {
     return( 
@@ -32,26 +33,29 @@ export default function Share() {
             boxShadow : "1px 1px 2px grey",
             border : "1px solid DCDCDC"}}/>
         </div>   
-        <div class = "wrapper">
+        <div class = "wrapper"  style = {{paddingLeft : "80px"}}>
  <div class = "category" style = {{
   fontSize : "23px",
   fontWeight: "bold",
   color : "#F85757",
+  marginBottom : "20px"
  }}>나눔받기</div>
  <div class = "title " style = {{fontSize : "45px"}}> 
  <p>무거운 가방 옮겨주실 분 찾아요</p>
  </div>
- <div class = "wrapper">
-        <img className="profile-img" style ={{borderRadius: "50%"}} src="/images/profile_image.png" alt="logo"/>
+ <div class = "wrapper" style = {{paddingTop : "40px", display : "inline-flex" , flexDirection : "row", alignItems: "center"}}>
+        <img className="profile-img" style ={{borderRadius: "50%",            
+        marginRight: "18px"}} src="/images/profile_image.png" alt="logo"/>
         <span className ="profile-name" style={{
-            fontSize : "25px"
+            fontSize : "25px",
+            paddingRight: "36px"
             }}> 김민지</span>
         <span className ="post-date" style={{
             fontSize : "25px",
             color: "#727272",
             }} >2021.4.21</span>
             </div>
-            <div class = "wrapper">
+            <div class = "wrapper" style = {{marginTop : "54px"}}>
  <button class = "delete-btn" type="submit" style = {{
              backgroundColor : "#FFFFFF",
              borderRadius : "25px",
@@ -61,7 +65,9 @@ export default function Share() {
              color: "#7E7979",
              borderColor : "#585858",
              borderWidth: "1px",
-            boxShadow : "2px 2px 2px #585858;"
+            boxShadow : "2px 2px 2px #585858",
+            marginRight : "36px",
+            textAlign : "center",
        }}>삭제하기</button>
           <Link href = '/share/share_form'><button class = "submit-btn" type="submit" style = {{
              backgroundColor : "#F85757",
@@ -71,7 +77,8 @@ export default function Share() {
              fontSize : "26px",
              color: "white",
              border : "none",
-             boxShadow : "2px 2px 2px #585858;"
+             boxShadow : "2px 2px 2px #585858;",
+             textAlign : "center",
        }}>글 등록하기</button></Link>
         </div> 
 
@@ -79,29 +86,28 @@ export default function Share() {
    </div>
    <Divider />
    <div class = "section-main">
-      <div class = "wrapper">
-      <DescriptionIcon/>        
-      <span style = {{ fontSize : "35px", fontWeight : "bold" }}>정보</span>
+      <div class = "wrapper" style ={{marginTop : "60px" , paddingBottom : "34px", }}>
+      <DescriptionIcon style={{ fontSize: 35 }}/>        
+      <span style = {{ fontSize : "35px", fontWeight : "bold", paddingLeft: "8px" ,color : "#1A1818"}}>정보</span>
       </div>
-        <div class = "desc" style = {{fontSize : "30px", color : "#1A1818"}}>제가 수업을 마쳤는데 가방이 너무 무겁네요 ; 옮겨주실분 구해요</div>
+        <div class = "desc" style = {{fontSize : "30px", color : "#1A1818",paddingBottom :"100px"}}>제가 수업을 마쳤는데 가방이 너무 무겁네요 ; 옮겨주실분 구해요</div>
         </div>
         <Divider />
         <div class ="section-bottom" style={{
            display: "flex",
            flexFlow : "column"
             }}>
-        <div class= "comment-num">
-            <span>댓글</span>
-            <span>1</span>
+        <div class= "comment-num" style = {{marginTop : "60px", marginBottom : "24px", fontSize : "30px"}} >
+            <span style = {{ color: "#1A1818", paddingRight : "8px"}} >댓글</span>
+            <span style = {{color: "#FF4B4B"}}>1</span>
         </div>
         <div class = "comment-form-box" style = {{ 
-            // flexFlow : 'row', 
-            // alignContent:"baseline" , 
             height: "70px", 
             width : "1129px",
             border: "1px  solid #7A7A7A", 
             backgroundColor : "white",
             borderWidth : "1px",
+            marginBottom : "60px"
             }}>
                 
             <input id = "comment-form" name="comment" type="text" placeholder="댓글을 입력하세요" style = {{  
@@ -120,14 +126,18 @@ export default function Share() {
              borderTop : "none",
              borderBottom : "none",
              justifySelf: "end",
-       }}><CreateIcon/>등록</button>
+             paddingRight : "36px",
+             textAlign : "center",
+       }}><span class = "icon"   style = {{paddingLeft : "8px", paddingRight: "8px"}}><CreateIcon/></span>등록</button>
       </div>
       <div class = "comment-box">
           <p style= {{fontSize : "30px"}}>제꺼도 같이 해주실 분 ^^..</p>
-          <div class = "wrapper">
-        <img className="profile-img" src="/images/profile_image.png" style ={{borderRadius: "50%"}} alt="logo"/>
+          <div class = "wrapper" style = {{ display : "inline-flex" , flexDirection : "row", alignItems: "center", width : "1129px"}}>
+          <img className="profile-img" style ={{borderRadius: "50%",            
+        marginRight: "18px"}} src="/images/profile_image.png" alt="logo"/>
         <span className ="profile-name" style={{
-            fontSize : "25px"
+            fontSize : "25px",
+            paddingRight: "36px"
             }}> 김민지</span>
         <span className ="post-date" style={{
             fontSize : "25px",
@@ -138,9 +148,9 @@ export default function Share() {
             border : "none",
              fontSize : "25px",
              color: "#1A1818",
-       }}>
-           <ReplyIcon />
-           댓글달기</button>
+             justifySelf: "flex-end"
+       }}>  <ReplyIcon /> <span>댓글달기</span>
+         </button>
             </div>
       </div>
     </div>
