c4b8d66863dbcfe68d1d840016c6b99c6ef904bd
ohh31 <saewon412@gmail.com>
2021-05-08 20:27:40 +0900

    feat : css 스타일 적용

--- pages/share/index.js
+++ pages/share/index.js
@@ -2,6 +2,8 @@ import "semantic-ui-css/semantic.min.css";
 import { Divider } from "semantic-ui-react";
 import React, { useEffect, useState } from "react";
 import Link from 'next/link'
+import css from "styled-jsx/css";
+
 
 export default function ShareForm() {
   const [category, setCategory] = useState("1");
@@ -32,7 +34,6 @@ export default function ShareForm() {
 
     return( 
     <article style={{
-      // width: "91.6%",
       margin: "0px",
       paddingTop : "50px",
       paddingLeft:"60px",
@@ -49,26 +50,24 @@ export default function ShareForm() {
             }} >
    
  <form action="/create_process" method="post" onSubmit={createPost}>
-   <div class= "post_top" style={{
+   <div class= "section-top" style={{
            display: "inline-flex"
             }}>
-     <div class = "post_img">
- <img className="product" src="/images/product_image.png" alt="logo" style={{
-              backgroundColor : "white",
-              height: "416px",
-              width: "404px",
-              boxShadow : "1px 1px 2px grey",
-              border : "1px solid DCDCDC"
-              
-            }} /></div>
-<div class = "post_info">
- <div class = "selection-wrap" style={{
-              background:"url('/images/selection_arrow.png') no-repeat 97% 50%/25px auto",
-              border : "1px solid #606060",
-              borderRadius : "10px",
-              width : "180px",
-              height : "52px",
-            }}>
+     <div class = "wrapper">
+        <img className="img-form" src="/images/product_image.png" alt="logo" style= {{ backgroundColor : "white",
+  height: "416px",
+  width: "404px",
+  boxShadow : "1px 1px 2px grey",
+  border : "1px solid DCDCDC"}}/>
+            </div>
+<div class = "wrapper">
+ <div class = "category-form" style = {{
+   background:"url('/images/selection_arrow.png') no-repeat 97% 50%/25px auto",
+   border : "1px solid #606060",
+   borderRadius : "10px",
+   width : "180px",
+   height : "52px",
+ }}>
  <select id="category" onChange={handleChange} value={category} 
             style={{
               width : "180px",
@@ -85,23 +84,18 @@ export default function ShareForm() {
       <option selected value = "1">나눔받기</option>
       <option value = "2">나눔하기</option>
     </select></div>
-      <p><input id = "title" name="title" type="text" style={{
-              backgroundColor : "white",
-              height: "92px",
-              width: "644px",
-              fontSize : "45px",
-              // marginLeft: "88px",
-              // marginRight: "88px",
-              zIndex:1,
-            }}></input></p>
-        <div class = "writer-info">
-        <img className="icon" src="/images/profile_image.png" alt="logo" style={{
-             borderRadius: "50%",
-            }}/>
-        <span style={{
+      <p><input id = "title-form" name="title" type="text" style = {{  backgroundColor : "white",
+  height: "92px",
+  width: "644px",
+  fontSize : "45px",
+  zIndex:1,
+  }}></input></p>
+        <div class = "wrapper">
+        <img className="profile-img" src="/images/profile_image.png" alt="logo"/>
+        <span className ="profile-name" style={{
             fontSize : "25px"
             }}> 김민지</span>
-        <span style={{
+        <span className ="post-date" style={{
             fontSize : "25px",
             color: "#727272",
             }} >2021.4.21</span>
@@ -109,39 +103,100 @@ export default function ShareForm() {
             </div>
             </div>
       <Divider />
-      <div class = "section-title">
-      <img className="icon" src="/images/outline_description_black_24dp.png" alt="logo" />
-        <span>정보</span>
+      <div class = "section-main">
+      <div class = "wrapper">
+      <img className="icon" src="/images/outline_description_black_24dp.png" alt="logo" style = {{ width : "35px", height : "35px"}}/>
+        <span style = {{ fontSize : "35px", fontWeight : "bold" }}>정보</span>
       </div>
-        <textarea id = "desc" name="desc" type="text" style={{
-              backgroundColor : "white",
-              height: "482px",
-              width: "1128px",
-              zIndex:1,
-            }} ></textarea>
-       <div class ="section-bottom" style={{
+        <textarea id = "desc-form" name="desc" type="text" style ={{
+           backgroundColor : "white",
+  height: "482px",
+  width: "1128px",
+  zIndex:1,
+  }}></textarea>
+        </div>
+        <div class ="section-bottom" style={{
            display: "inline-flex"
             }}>
-       <Link href = '/' class = "cancel-button" ><div style={{
-              backgroundColor : "white",
-              border : "none",
-              fontSize : "26px",
-              color: "black",
-              textDecoration : "underline",
-            }} >취소하기</div></Link>
-        <button class = "submit-button" type="submit" style={{
-              backgroundColor : "#F85757",
-              borderRadius : "25px",
-              height: "63px",
-              width: "273px",
-              fontSize : "26px",
-              color: "white"
-            }} >글 등록하기</button>
+       <Link href = '/' class = "btnStyle.cancel-btn" >
+          <div style= {{ 
+         backgroundColor : "white",
+        border : "none",
+        fontSize : "35px",
+        color: "#7E7979",
+        textDecoration : "underline",
+        }}>취소하기</div></Link>
+       <button class = "submit-btn" type="submit" style = {{
+             backgroundColor : "#F85757",
+             borderRadius : "25px",
+             height: "63px",
+             width: "273px",
+             fontSize : "26px",
+             color: "white",
+       }}>글 등록하기</button>
         </div>
     </form>
+    {/* <style global jsx>{imgStyle}</style>
+    <style global jsx>{formStyle}</style>
+    <style global jsx>{btnStyle}</style> */}
 </div>
 </article>
-
 );
 
 ``}
+
+// const btnStyle = css`
+//   .cancel-btn{
+//     background-color : "white";
+//     border : "none";
+//     font-size : "26px";
+//     color: "black";
+//     text-decoration : "underline";
+//   }
+//   .submit-btn{
+//     background-color : "#F85757";
+//     border-radius : "25px";
+//     height: "63px";
+//     width: "273px";
+//     font-size : "26px";
+//     color: "white";
+//   }
+// `;
+// const imgStyle = css`
+//   .profile-img{
+//     border-radius: "50%",
+//   }
+// `;
+
+// const formStyle = css`
+// .img-form {
+//   background-color : "white";
+//   height: "416px";
+//   width: "404px";
+//   box-shadow : "1px 1px 2px grey";
+//   border : "1px solid DCDCDC";
+// }
+
+// .category-form{
+//   background:"url('/images/selection_arrow.png') no-repeat 97% 50%/25px auto";
+//   border : "1px solid #606060";
+//   border-radius : "10px";
+//   width : "180px";
+//   height : "52px";
+// }
+
+// #title-form{
+//   background-color : "white";
+//   height: "92px";
+//   width: "644px";
+//   font-size : "45px";
+//   z-index:1;
+// }
+
+// #desc-form{
+//   background-color : "white";
+//   height: "482px";
+//   width: "1128px";
+//   z-index:1;
+// }
+// `;
