1e782ee45dd03d94906d60df477af03ae5cfa3e5
ohh31 <saewon412@gmail.com>
2021-05-09 16:53:19 +0900

    docs : post background 파일명 수정

---
+++ pages/share/components/post_bg.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function PostBg({ children }) {
+  return (
+    <article
+      style={{
+        margin: "0px",
+        paddingTop: "50px",
+        paddingLeft: "60px",
+        paddingRight: "60px",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          height: "1525px",
+          margin: "0px",
+          paddingTop: "85px",
+          paddingLeft: "65px",
+          paddingRight: "65px",
+          zIndex: 1,
+        }}
+      >
+        {" "}
+        {children}
+      </div>
+    </article>
+  );
+}
+export default PostBg;
