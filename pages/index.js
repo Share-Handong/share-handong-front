3f323c42ae8129e723bc2d33f7ea724662ac12d5
김민지 <21600069@handong.edu>
2021-06-05 19:51:16 +0900

    fix: 시작 할 때 메인페이지로 가도록

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import { Button } from '@material-ui/core';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 // import "bootstrap";
 // import "bootstrap/dist/css/bootstrap.css";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Share-handong-front</h1>
-            <Link href="/share">
-                <Button />
-            </Link>
-        </div>
-    );
+    const router = useRouter();
+    useEffect(() => {
+        router.push('/main');
+    });
+
+    return <div />;
 }
